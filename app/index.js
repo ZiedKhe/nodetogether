ba4d3bcd70a2dd239ddf5e830047692007234b7f
Zied Kheriji <zied.web@gmail.com>
2016-04-15 00:27:19 +0200

    Add layouts, design and images - end of class

--- app/index.js
+++ app/index.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var app = express();
+var pickOne = require('pick-one');
+
+
+const motivations = require('motivations');
+var exphbs = require('express-handlebars');
+const catImages = [1, 2, 3, 4, 5, 6];
+
+app.use(express.static('./app/public'));
+
+// Create the vie engine Handlebars
+app.engine('handlebars', exphbs({            // we create an engin called handlebars
+    defaultLayout: "main",
+    layoutsDir: "./app/views/layouts"        // the layout directory is starting from the root of the project
+}))
+
+
+app.set('views', './app/views');
+app.set('view engine', 'handlebars');
 
 app.get("/", function(request,response){ 
-    response.send ("here is the homepage");
+    //response.send ("here is the homepage");
+    motivation = pickOne(motivations);
+    image = pickOne(catImages);
+    response.render('motivation', { motivation, image });
+
 });
 
+
 app.get("/about", function(request,response){ 
-    response.send ("about page");
+    response.render ("about page");
 });
 
 module.exports = app;
